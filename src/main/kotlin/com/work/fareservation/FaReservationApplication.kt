53491b6128615fd8123cb1cdc28c65b9eb05338d
한성길 <zaien24@gmail.com>
2025-04-28 12:51:34 +0900

    init

---
+++ src/main/kotlin/com/work/fareservation/FaReservationApplication.kt
@@ -0,0 +1,11 @@
+package com.work.fareservation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FaReservationApplication
+
+fun main(args: Array<String>) {
+    runApplication<FaReservationApplication>(*args)
+}
---
+++ src/test/kotlin/com/work/fareservation/FaReservationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.work.fareservation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FaReservationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
